0f2dfd2420e4825b55ccc8f886396feaeb34d62e
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-12-21 16:50:39 +0100

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var webserver = require('gulp-webserver');
+var open = require('gulp-open');
+
+
+gulp.task('default', function () {
+
+   gulp.src('src/js/*.js')
+    .pipe(babel({
+
+        presets: ['es2015']
+
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+
+   gulp.src('src/*.html')
+     .pipe(gulp.dest('dist'));
+
+  gulp.src('dist')
+    .pipe(webserver({
+      port: 1234
+    }));
+
+  gulp.src('./dist/index.html')
+    .pipe(open());
+
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Data Viz D3
+
+## author: Lukas Aichbauer, Bianca Leitner

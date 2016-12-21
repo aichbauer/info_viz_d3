d75ceb966c1d8bbee2a9cf555e5520793f45ef60
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-12-21 17:08:12 +0100

    update: gulpfile, sass; update: package, sass;

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,19 @@ var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
 var webserver = require('gulp-webserver');
 var open = require('gulp-open');
+var sass = require('gulp-sass');
 
 
 gulp.task('default', function () {
 
-   gulp.src('src/js/*.js')
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+
+  gulp.src('src/assets/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+
+  gulp.src('src/assets/js/*.js')
     .pipe(babel({
 
         presets: ['es2015']
@@ -15,10 +23,7 @@ gulp.task('default', function () {
       })
     )
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
-
-   gulp.src('src/*.html')
-     .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/assets.js'));
 
   gulp.src('dist')
     .pipe(webserver({

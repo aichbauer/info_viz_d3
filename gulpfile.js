8d2f2b09faf60d89863c04a9d3c7eb041bec42e3
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-12-22 01:36:56 +0100

    update: gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,46 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var babel = require('gulp-babel');
-var sass = require('gulp-sass');
-var server = require('gulp-server-livereload');
+var gulp    = require('gulp');
+var uglify  = require('gulp-uglify');
+var babel   = require('gulp-babel');
+var sass    = require('gulp-sass');
+var server  = require('gulp-server-livereload');
 
+gulp.task('default', ['htmlTask','sassTask','jsTask']);
 
-gulp.task('default', ['html_task','sass_task','js_task']);
+gulp.task('serve', ['htmlTask','sassTask','jsTask'], function(){
 
-gulp.task('sass_task',function () {
+  gulp.src('./dist')
+    .pipe(server({
 
-  return gulp.src('src/assets/scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/assets/css'));
+      livereload: true,
+      open:true
+
+    })
+  )
+
+  gulp.watch('./src/*.html', ['htmlTask']);
+  gulp.watch('./src/assets/scss/*.scss', ['sassTask']);
+  gulp.watch('./src/assets/js/*.js', ['jsTask']);
 
 });
 
-gulp.task('html_task',function () {
+gulp.task('sassTask',function () {
 
-  return gulp.src('src/*.html')
+  return gulp.src('./src/assets/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/assets/css'));
+
+});
+
+gulp.task('htmlTask',function () {
+
+  return gulp.src('./src/*.html')
     .pipe(gulp.dest('dist'));
 
 });
 
-gulp.task('js_task',function () {
+gulp.task('jsTask',function () {
 
-  return gulp.src('src/assets/js/*.js')
+  return gulp.src('./src/assets/js/*.js')
     .pipe(babel({
 
         presets: ['es2015']
@@ -32,20 +48,6 @@ gulp.task('js_task',function () {
       })
     )
     .pipe(uglify())
-    .pipe(gulp.dest('dist/assets/js'));
+    .pipe(gulp.dest('./dist/assets/js'));
 
-});
-
-gulp.task('webserver', function () {
-  return gulp.src('dist')
-    .pipe(server({
-      livereload: true,
-      open:true
-    }))
-})
-
-gulp.task('watch', ['webserver'], function () {
-  gulp.watch('src/*.html', ['html_task']);
-  gulp.watch('src/assets/scss/*.scss', ['sass_task']);
-  gulp.watch('src/assets/js/*.js', ['js_task']);
 });

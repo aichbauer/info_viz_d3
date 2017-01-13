201ce4bb98491d291877d4aa1fa70019731f7fc8
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-01-13 17:52:56 +0100

    update: linechart legend

--- src/assets/js/components/charts/LineChart.js
+++ src/assets/js/components/charts/LineChart.js
@@ -20,32 +20,19 @@ class LineChart {
   }
 
 
-  render(new_data, loc = ['New York', 'Hawaii'], crime = 'Violent.crime.number.rate') {
+  render(new_data, loc = 'New York', valueRate = 'rate') {
 
-    let newData = [];
+    let crimesArray = Object.keys(new_data[0].crimes.years['2009']);
+    crimesArray.shift();
 
-    for (let i in new_data) {
+    if (valueRate == 'rate') crimesArray = crimesArray.filter(/./.test.bind(new RegExp('.rate')));
+    else if (valueRate == 'abs') {
 
-      if (loc.includes(new_data[i].location)) {
+      for (let x in crimesArray) {
 
-        for (let x in new_data[i].crimes.years) {
+        if (crimesArray[x].includes('.rate')) {
 
-          console.log(new_data[i].location)
-          console.log('\n\n')
-          console.log(new_data[i].crimes)
-          console.log('\n\n')
-          console.log(new_data[i].crimes.years[x][crime])
-          console.log(Object.keys(new_data[i].crimes.years))
-          console.log('\n\n\n\n')
-
-          let crimeYear = {
-            "location": new_data[i].location,
-            "crimeValue": new_data[i].crimes.years[x][crime],
-            "year": x,
-
-          }
-
-          newData.push(crimeYear);
+          crimesArray.splice(x, 1);
 
         }
 
@@ -53,36 +40,79 @@ class LineChart {
 
     }
 
-    console.log(newData);
+    let newData = [];
+    let cri;
+
+    for (let i in new_data) {
+
+      if (loc == new_data[i].location) {
+
+        for (let y of crimesArray) {
+
+          for (let x in new_data[i].crimes.years) {
+
+            let crimeYear = {
+              "crime": y,
+              "crimeValue": new_data[i].crimes.years[x][y],
+              "year": x,
+
+            }
+
+            newData.push(crimeYear);
+
+          }
+
+        }
+
+
+      }
+
+    }
 
     var dataGroup = d3.nest()
-      .key(function (d) { return d.location; })
+      .key(function (d) { return d.crime; })
       .entries(newData);
-    console.log(JSON.stringify(dataGroup));
+
     var color = d3.scaleOrdinal(d3.schemeCategory20);
+
     var vis = d3.select(this.div).append('svg')
       .attr('width', this.width + this.margin.left + this.margin.right)
       .attr('height', this.height + this.margin.top + this.margin.bottom)
       .attr('class', 'svg-linechart');
+
     let xScale = d3.scaleLinear().range([this.margin.left, this.width - this.margin.right]).domain([d3.min(newData, function (d) {
+
       return d.year;
+
     }), d3.max(newData, function (d) {
+
       return d.year;
-    })])
+
+    })]);
+
     let yScale = d3.scaleLinear().range([this.height - this.margin.top, this.margin.bottom]).domain([d3.min(newData, function (d) {
+
       return d.crimeValue;
+
     }), d3.max(newData, function (d) {
+
       return d.crimeValue;
+
     })])
+
     let xAxis = d3.axisTop()
       .scale(xScale)
+
     let yAxis = d3.axisLeft()
       .scale(yScale)
+
     let lSpace = this.width / dataGroup.length;
+
     vis.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + (this.height - this.margin.bottom) + ")")
       .call(xAxis);
+
     vis.append("g")
       .attr("class", "y axis")
       .attr("transform", "translate(" + (this.margin.left) + ",0)")
@@ -90,42 +120,45 @@ class LineChart {
 
     var lineGen = d3.line()
       .x(function (d) {
+
         return xScale(d.year);
+
       })
       .y(function (d) {
+
         return yScale(d.crimeValue);
+
       })
-    dataGroup.forEach(function (d, i) {
+
+    for (let i in dataGroup) {
+
       vis.append('svg:path')
-        .attr('d', lineGen(d.values))
+        .attr('d', lineGen(dataGroup[i].values))
         .attr('stroke', function (d, j) {
+
           return "hsl(" + Math.random() * 360 + ",100%,50%)";
+
         })
         .attr('stroke-width', 2)
-        .attr('id', 'line_' + d.key)
+        .attr('id', 'line_' + dataGroup[i].key)
         .attr('fill', 'none');
 
-      vis.append("text")
-        .attr("x", (lSpace / 2) + i * lSpace)
-        .attr("y", this.height)
-        .style("fill", "black")
-        .attr("class", "legend")
-        .on('click', function () {
-          var active = d.active ? false : true;
-          var opacity = active ? 0 : 1;
-          d3.select("#line_" + d.key).style("opacity", opacity);
-          d.active = active;
-        })
-        .text(d.key);
+    }
 
-    });
+    for (let i in dataGroup){
+      console.log(dataGroup[i].key);
+    }
+
+
+       
 
   }
 
 
-  filter() {
 
-  }
+filter() {
+
+}
 
 }
 
